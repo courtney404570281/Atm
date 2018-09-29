3375e82733c8162ce88b7ea6bbb2f371dad012fc
Teresa Liang <teresa110213@gmail.com>
2018-09-29 15:53:24 +0800

    add GridView

--- app/src/main/java/com/atm/atm/Functions.java
+++ app/src/main/java/com/atm/atm/Functions.java
@@ -3,6 +3,7 @@ package com.atm.atm;
 public class Functions {
 
     String name;
+    int icon;
 
     public Functions() {
     }
@@ -11,11 +12,24 @@ public class Functions {
         this.name = name;
     }
 
+    public Functions(String name, int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
     }
 }
--- app/src/main/java/com/atm/atm/MainActivity.java
+++ app/src/main/java/com/atm/atm/MainActivity.java
@@ -2,22 +2,32 @@ package com.atm.atm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_LOGIN = 100;
+    private static final String TAG = MainActivity.class.getSimpleName();
     boolean logon = false;
-    String[] functions = null;
+    private List<Functions> functions;
+    //    String[] functions = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +53,91 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // RecyclerView
+        setupFunctions();
+
+
+
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager((new GridLayoutManager(this, 3)));
+
 
         // Adapter
-        FunctionAdapter adapter = new FunctionAdapter(this);
+//        FunctionAdapter adapter = new FunctionAdapter(this);
+        IconAdapter adapter = new IconAdapter();
         recyclerView.setAdapter(adapter);
 
+    }
+
+    private void setupFunctions() {
+        functions = new ArrayList<>();
+        String[] funcs = getResources().getStringArray(R.array.functions);
+        functions.add(new Functions(funcs[0], R.drawable.func_transaction));
+        functions.add(new Functions(funcs[1], R.drawable.func_balance));
+        functions.add(new Functions(funcs[2], R.drawable.func_finance));
+        functions.add(new Functions(funcs[3], R.drawable.func_contacts));
+        functions.add(new Functions(funcs[4], R.drawable.func_exit));
+    }
+
+    public class IconAdapter extends RecyclerView.Adapter<IconAdapter.IconHolder> {
+        @NonNull
+        @Override
+        public IconHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = getLayoutInflater().inflate(R.layout.item_icon, viewGroup, false);
+            return new IconHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull IconHolder iconHolder, int i) {
+            final Functions function = functions.get(i);
+            iconHolder.nameText.setText(function.getName());
+            iconHolder.iconImage.setImageResource(function.getIcon());
+
+            iconHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemClicked(function);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return functions.size();
+        }
+
+        public class IconHolder extends RecyclerView.ViewHolder {
+
+            ImageView iconImage;
+            TextView nameText;
+
+            public IconHolder(@NonNull View itemView) {
+                super(itemView);
+
+                iconImage = itemView.findViewById(R.id.item_icon);
+                nameText = itemView.findViewById(R.id.item_name);
+            }
+        }
+    }
+
+    private void itemClicked(Functions function) {
+        Log.d(TAG, "itemClicked: " + function.getName());
+
+        switch (function.getIcon()){
+            case R.drawable.func_transaction:
+                break;
+            case R.drawable.func_balance:
+                break;
+            case R.drawable.func_finance:
+                break;
+            case R.drawable.func_contacts:
+                break;
+            case R.drawable.func_exit:
+                finish();
+                break;
+
+        }
     }
 
     @Override
